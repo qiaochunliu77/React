a73a956d886e8617162e83b96c7772061042dfa7
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-05-15 11:15:21 +0800

    5.15

---
+++ react-comment-app/src/CommentApp.js
@@ -0,0 +1,13 @@
+import React ,{Component }from 'react'; //按需加载
+
+// react 来自于facebook 继承component就拥有了这个组件
+class CommentApp extends Component {
+    render(){ //接口
+        return(
+            <div>
+                CommentApp
+            </div>
+        )
+    }
+}
+export default CommentApp

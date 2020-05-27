1beac7ce26246b251cd04610e6a6919b49cc5c71
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-05-27 22:35:52 +0800

    5.27

--- do-document-redux/src/index.js
+++ do-document-redux/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// 数据管理模式
+// 创建中央仓库 数据管理的架构 
+// createStore comments 放在中央仓库
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// 数据就放到仓库里 跟组件没有依赖关系
+import CommentApp from './containers/CommentApp';
+import commentsReducer from './reducers/comments';
+// 仓库 单一 状态树 
+const store = createStore(commentsReducer);
+
+ReactDOM.render (
+  <Provider store = {store}>
+    <CommentApp />
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+// react-router react-redux
---
+++ do-document-redux/src/reducers/comments.js
@@ -0,0 +1,15 @@
+// reducer 就是函数
+let comments = [
+    {
+      "id": 1,
+      "username": "lqc",
+      "content": "宝宝"
+    }
+  ];
+
+  export default function(state = {
+    comments: comments
+  }) {
+    //   初始状态
+    return state
+  }

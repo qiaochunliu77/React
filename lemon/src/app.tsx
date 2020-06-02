75c22a56e225babfb82a867bbc721c22bb5b7603
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-06-02 09:28:14 +0800

    使用webpack typescript搭建react应用

--- lemon/src/app.tsx
+++ lemon/src/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Header, About } from './components';
 
-
+// StatelessComponent无状态组件 
 export const App: React.StatelessComponent<{}> = () => {
   return (
     <div className="container-fluid">

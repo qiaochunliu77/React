cbf3571a32bdf8766528c1a3ca0afa6ebac33535
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-06-03 16:17:23 +0800

    propstypes ref获取dom 路由 开发流程

--- do-beer/README.md
+++ do-beer/README.md
@@ -4,3 +4,14 @@
 语义化设计URL /post/:postId
 
 1. 把应用架构好 路由
+2. Main -> loadBeers => loading => list
+3. search/:searchTerm 路由 ->rule -> 显示
+4. search/:new
+
+
+注：
+1. es6箭头函数this会指向外层
+2. this.searchRef.current.value 获取input输入框的值 代替onchange事件
+3. this.context.router.history.push(`/search/${searchTerm}`)
+    形成能够跳转的新的地址
+4. this.props.match.params 取得动态参数 :searchTerm
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,45 +1,10 @@
+import wrapWithLoadData from './wrapWithLoadData'
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-
-
-class Card extends Component {
+class InputWithUserName extends Component {
   render () {
-    return (
-      <div className='card'>
-        <div className='card-content'>
-          {this.props.children}
-        </div>
-      </div>
-    )
+    return <input value={this.props.data} />
   }
 }
 
-ReactDOM.render(
-  <Card>
-    <h2>React.js 小书</h2>
-    <div>开源、免费、专业、简单</div>
-    订阅：<input />
-  </Card>,
-  document.getElementById('root')
-)
-
-
-
-
-
-
-
-
-// 把组件渲染并且构造 DOM 树
-// 插入到页面上上id 为 root 的 div 元素）
-
-// React.createElement 中实例化一个 Header
-// const header = new Header(props, children)
-// // React.createElement 中调用 header.render 方法渲染组件的内容
-// const headerJsxObject = header.render()
-
-// // ReactDOM 用渲染后的 JavaScript 对象来来构建真正的 DOM 元素
-// const headerDOM = createDOMFromObject(headerJsxObject)
-// // ReactDOM 把 DOM 元素塞到页面上
-// document.getElementById('root').appendChild(headerDOM)
+InputWithUserName = wrapWithLoadData(InputWithUserName, 'username')
+export default InputWithUserName

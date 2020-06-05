28881a2695fb15c109323baa21a34baf1fa189e0
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-06-05 23:46:19 +0800

    mango music

---
+++ mango-music-chapter/src/components/search/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Search.styl';
+
+class Search extends React.Component {
+    render() {
+        return (
+           <div>Search</div>
+        )
+    }
+}
+
+export default Search;

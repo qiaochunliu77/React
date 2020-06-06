3aa31f3f96e441c464ac6f3d42355c82155f10ac
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-06-06 21:12:51 +0800

    demo mango music

--- mango-music-chapter/src/components/recommend/Recommend.js
+++ mango-music-chapter/src/components/recommend/Recommend.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Recommend.styl'; // webpack
 import Swiper from 'swiper';
-import 'swiper/css/swiper.min.css'
+import "swiper/css/swiper.min.css";
+import Loading from '../../common/loading/Loading';
+// 应用中很多图片
+// import Lazyload from 'react-lazyload'; // 图片延迟加载
+// 1. 路由   
+// 2. redux
+// 3. 切页面+ js 
+// 4. 生命周期 + api 
+// 5. 公共组件
+
+
+// 所有的数据请求都放到api目录下
+import { getNewAlbum }  from '../../api/recommend';
+import LazyLoad from 'react-lazyload';
+
 
 // 1. 幻灯片， swiper
-// 2. 加入swiper功能
+// 2. 加入 swiper功能
 // 数据   { src, link}
 class Recommend extends React.Component {
   constructor() {
@@ -13,6 +27,8 @@ class Recommend extends React.Component {
     // 1. 用假数据 把页面先做出
     // 2. 未来再改成接口
     this.state = {
+      newAlbums: [], /* 数据驱动的界面 */
+      loading: true,
       sliderList: [{
         id: 1,
         picUrl: 'https://mat1.gtimg.com/rain/bailing20/4333e6a9ac25.uzi.png',
@@ -29,18 +45,54 @@ class Recommend extends React.Component {
     }
   }
   componentDidMount() {
-      new Swiper('.slider-container',{
-          loop:true,
-          autoplay: {
-              delay: 1000,
-          },
-          pagination: {
-              el:'.swiper-pagrination',
-              type:'bullets',
-          }
+    new Swiper(".slider-container", {
+      loop: true,
+      autoplay: {
+        delay: 1000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      }
+    })
+
+
+    // 获取最新专辑 功能的封装
+    // fetch  低级的
+    getNewAlbum() /**promise */
+      .then(res => {
+        // console.log(res)
+        this.setState({
+          loading: false,
+          newAlbums: res
+        })
       })
+    // setTimeout(() => {
+    //   this.setState({
+    //     loading: false
+    //   })
+    // }, 3000)
   }
   render() {
+    // 切页面
+    let albums = this.state.newAlbums.map(item => (
+      <div className="album-wrapper" key={item.id}>
+        <div className="left">
+          <img src={item.img} alt={item.name} width="100%" height="100%"/>
+        </div>
+        <div className="right">
+          <div className="album-name">
+            {item.name}
+          </div>
+          <div className="singer-name">
+            {item.singer}
+          </div>
+          <div className="public-time">
+            {item.publicTime}
+          </div>
+        </div>
+      </div>
+    ))
     return (
       <div className="music-recommend">
         <div className="slider-container">
@@ -50,7 +102,9 @@ class Recommend extends React.Component {
                 return (
                   <div className="swiper-slide" key={slider.id}>
                     <a href={slider.linkUrl} className="slider-nav">
-                      <img src={slider.picUrl} alt="" width="100%" height="100%"/>
+                      <LazyLoad height={60}>
+                        <img src={slider.picUrl} alt="" width="100%" height="100%"/>
+                      </LazyLoad>
                     </a>
                   </div>
                 );
@@ -59,6 +113,15 @@ class Recommend extends React.Component {
           </div>
           <div className="swiper-pagination"></div>
         </div>
+
+
+        <div className="album-container">
+          <h1 className="title">最新专辑</h1>
+          <div className="album-list">
+            {albums}
+          </div>
+        </div>
+        <Loading show={this.state.loading} title="正在加载..."/>
       </div>
     )
   }

09970c6f64dd8dd5bfb6f31603c3e4f84f45bbb1
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-06-23 23:47:32 +0800

    6.23

--- mango-music-chapter/src/api/recommend.js
+++ mango-music-chapter/src/api/recommend.js
@@ -5,7 +5,8 @@ export function getNewAlbum() {
       g_tk: 1278911659,
       hostUin: 0,
       platform: "yqq",
-      needNewCode: 0,      data: `{"albumlib":
+      needNewCode: 0,
+      data: `{"albumlib":
   {"method":"get_album_by_tags","param":
   {"area":1,"company":-1,"genre":-1,"type":-1,"year":-1,"sort":2,"get_tags":1,"sin":0,"num":50,"click_albumid":0},
   "module":"music.web_album_library"}}`

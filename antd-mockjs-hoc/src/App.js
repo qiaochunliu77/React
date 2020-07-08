68c688f57cd1d4f2fc3b14101519a71ddcfd534b
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-07-08 12:05:04 +0800

    7.8

--- antd-mockjs-hoc/src/App.js
+++ antd-mockjs-hoc/src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Table} from 'antd';
+// import 'antd/dist/antd.min.css';
+// antd全局引入方式 babel
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={dataSource} columns={columns} />;
     </div>
   );
 }
